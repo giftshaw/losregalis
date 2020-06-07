655d0afb15b6f369e5c50732f5ed244780f0b1ad
Gift <49137251+giftshaw@users.noreply.github.com>
2020-06-07 18:54:29 +0100

    navbar added

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,17 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  // {
+  //   path: '/services',
+  //   name: 'Services',
+  //   component: Services
+  // },
+  // {
+  //   path: '/contact',
+  //   name: 'Contact Us',
+  //   component: Contact
+  // }
 ]
 
 const router = new VueRouter({
