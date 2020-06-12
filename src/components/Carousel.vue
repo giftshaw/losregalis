ed371c2b108b2fb444e61465ca62c5a2b9dddd5e
unknown <nazihahmed55@gmail.com>
2020-06-12 12:12:15 +0100

    Added the carousel component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="carousel">
+        <v-carousel hide-delimiters
+        show-arrows-on-hover
+        height="500"
+        cycle="true"
+        dark="true"
+        interval="6000">
+          <v-carousel-item v-for="(item,index) in items" :key="index" :src="item.image"
+    ></v-carousel-item>
+  </v-carousel>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }, 
+
+  data(){
+    return {
+        items: [
+        {
+          image: require("@/assets/caurosel/slideone.jpg")
+        },
+        {
+          image: require("@/assets/caurosel/slidetwo.jpg")
+        },
+        {
+          image: require("@/assets/caurosel/slidethree.jpg")
+        },
+        {
+          image: require("@/assets/caurosel/slidefour.jpeg")
+        }
+      ]
+    }
+  }
+};
+</script>
